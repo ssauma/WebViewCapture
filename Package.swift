ac2a2f824d65f3cfb4f332f9a89d596ea663efef
Juyeon Lee <juyeonlee@timerit.com>
2022-01-07 17:53:55 +0900

    no message

--- Package.swift
+++ Package.swift
@@ -6,24 +6,13 @@ import PackageDescription
 let package = Package(
     name: "WebViewCapture",
     platforms: [
-            .iOS(.v11)
-        ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "WebViewCapture",
-            targets: ["WebViewCapture"]),
+        .iOS(.v11)
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    products: [
+        .library(name: "WebViewCapture", targets: ["WebViewCapture"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "WebViewCapture",
-            dependencies: [])
+        .target(name: "WebViewCapture", path: "Sources/WebViewCapture")
     ],
     swiftLanguageVersions: [
         .v5
